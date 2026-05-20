460bd3e802ff22a650c9f04b1ba93a200bc529d0
Andy Wick <awick@purple.org>
2026-05-20 13:27:48 -0400

    Bump to v6.4.0

--- Formula/arkime-cont3xt.rb
+++ Formula/arkime-cont3xt.rb
@@ -1,8 +1,8 @@
 class ArkimeCont3xt < Formula
   desc "Service wrapper for Arkime Cont3xt"
   homepage "https://arkime.com"
-  url "https://github.com/arkime/arkime/archive/refs/tags/v6.3.1.tar.gz"
-  sha256 "fa3b27adce7e2a5367dc0a0d3899059e54ee3cfdce643559fd9516d069bc7b1b"
+  url "https://github.com/arkime/arkime/archive/refs/tags/v6.4.0.tar.gz"
+  sha256 "2b8653a8e5cb47f86f192eb144ff0ef35b113a974fd90d5e7fce11abfe300c96"
   license "Apache-2.0"
 
   depends_on "arkime/arkime/arkime"
